d70f6f3b057095389ede265d1786af59b206b7b9
Jenny Johns <johns.j@husky.neu.edu>
2017-12-10 20:05:54 -0500

    editing objects

--- src/app/components/edit-object/edit-object.component.ts
+++ src/app/components/edit-object/edit-object.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from "../../services/user.service.client";
+import {CBService} from "../../services/cb.service.client";
+import {CEService} from "../../services/ce.service.client";
+import {PubService} from "../../services/pub.service.client";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-edit-object',
@@ -6,10 +11,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-object.component.css']
 })
 export class EditObjectComponent implements OnInit {
+  objectId: String;
+  objectType: String;
 
-  constructor() { }
+  constructor(private userService: UserService,
+              private cbService: CBService,
+              private ceService: CEService,
+              private pubService: PubService,
+              private router: Router,
+              private activatedRoute: ActivatedRoute) {
+  }
 
   ngOnInit() {
+    this.activatedRoute.params
+      .subscribe((params: any) => {
+        this.objectId = params['objId'];
+      });
+    if (this.userService.findUserById(this.objectId) !== null) {
+      this.objectType = 'User';
+    } else if (this.cbService.findCBbyId(this.objectId) !== null) {
+      this.objectType = 'Celestial Body';
+    } else if (this.ceService.findCEbyId(this.objectId) !== null) {
+      this.objectType = 'Celestial Event';
+    } else if (this.pubService.findPubbyId(this.objectId) !== null) {
+      this.objectType = 'Publication';
+    }
   }
 
 }
